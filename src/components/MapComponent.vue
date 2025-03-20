017666e68419198789ce19943abda52e5361307f
Kyrylo Tarasenko <gotoredo@gmail.com>
2025-03-20 11:49:54 +0200

    create basic boilerplate

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>Map</section>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
+import 'vue3-openlayers/styles.css'
 import './assets/main.css'
+
+import OpenLayersMap from 'vue3-openlayers'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(OpenLayersMap)
 
 app.mount('#app')
